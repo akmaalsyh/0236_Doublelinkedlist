802c26b56f8b8bfabe674ba955f4f2c9ce50d7de
akmaalsyh <taufansyah50@gmail.com>
2024-05-02 15:36:11 +0700

    memasukkan void add node step 1

--- 0236_doublelinkedlist.cpp
+++ 0236_doublelinkedlist.cpp
@@ -11,3 +11,12 @@ struct Node
 };
 
 Node* START = NULL;
+
+void addNode()
+{
+    Node* newNode = new Node(); // step1: create a new node
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs;      // Assign value to the date field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;       // Assign value to the date field of the new node 
+};
