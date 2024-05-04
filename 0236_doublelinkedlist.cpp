974e09b2dd375d69736e084445f93e47c190034d
akmaalsyh <taufansyah50@gmail.com>
2024-05-04 10:49:07 +0700

    memasukkan void revtraverse while

--- 0236_doublelinkedlist.cpp
+++ 0236_doublelinkedlist.cpp
@@ -164,6 +164,14 @@ void revtraverse()
     else
     {
         cout << "\nRecords is descending order of roll number are:" << endl;
+        Node *currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
         
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
     }
 }
